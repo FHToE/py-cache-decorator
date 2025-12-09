84cb4e62db0242f2b312926ee82f828d141af608
m.oleinik89@gmail.com <m.oleinik89@gmail.com>
2025-12-09 11:42:17 +0200

    Fix key generation, fix key existence check

--- app/main.py
+++ app/main.py
@@ -4,19 +4,13 @@ from typing import Callable, Any
 def cache(func: Callable) -> Callable:
     cache_dict = {}
 
-    def get_key_by_args(*args, **kwargs) -> str:
-        cache_key = ""
-        for arg in args:
-            cache_key += str(arg)
-        for key, value in kwargs.items():
-            cache_key += f"{str(key)}{str(value)}"
-
-        return cache_key
+    def get_key_by_args(*args, **kwargs) -> tuple:
+        return args, tuple(sorted(kwargs.items()))
 
     def inner(*args, **kwargs) -> Any:
         cache_key = get_key_by_args(*args, **kwargs)
 
-        if cache_dict.get(cache_key) is None:
+        if cache_key not in cache_dict:
             print("Calculating new result")
             result = func(*args, **kwargs)
             cache_dict[cache_key] = result
